f48f0190dfaf4686e5af7bc6717cc8800d8e3dfe
Rafael Agredano <rafaelagredano99@gmail.com>
2025-02-22 11:48:18 -0600

    main, env were created

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/mydatabase")
+
+client = AsyncIOMotorClient(MONGO_URI)
+db = client.Cluster007
+
 from fastapi import FastAPI
 
 app = FastAPI()
